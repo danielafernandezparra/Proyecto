b2cebf6c67d8ce55f701a0c8eea72eab049de6b0
Daniela Fernandez <daniela.fernandez7@gmail.com>
2019-08-11 22:25:25 -0400

    Routes

--- config/routes.rb
+++ config/routes.rb
@@ -1,7 +1,10 @@
 Rails.application.routes.draw do
+  devise_for :users
+  devise_for :user do
+    root to: "devise/sessions#new"
+  end
+  resources :roles
   resources :quotations
   resources :customers
-  devise_for :users
-  resources :roles
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
 end
